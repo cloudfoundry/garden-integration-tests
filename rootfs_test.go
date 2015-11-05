ebc2db0bc8f701b7f5d96f2223a72442d80d3b9f
Luan Santos <lsantos@pivotal.io>
2015-11-05 14:16:19 +0000

    Fix rootfs tests that use cloudfoundry/with-volume docker image
    Signed-off-by: Svett Ralchev <sralchev@pivotal.io>

--- rootfs_test.go
+++ rootfs_test.go
@@ -24,7 +24,7 @@ var _ = Describe("Rootfses", func() {
 			JustBeforeEach(func() {
 				process, err := container.Run(garden.ProcessSpec{
 					User: "root",
-					Path: "/usr/sbin/adduser",
+					Path: "adduser",
 					Args: []string{"-D", "bob"},
 				}, garden.ProcessIO{
 					Stdout: GinkgoWriter,
