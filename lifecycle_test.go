b6417eeac81b97ac5dfdb27c7a205a66bb3f8f8b
Svett Ralchev <sralchev@pivotal.io>
2015-11-05 14:19:28 +0000

    Fix lifecycle test that uses cloudfoundry/with-volume image
    Signed-off-by: Luan Santos <lsantos@pivotal.io>

--- lifecycle_test.go
+++ lifecycle_test.go
@@ -120,7 +120,7 @@ var _ = Describe("Lifecycle", func() {
 			JustBeforeEach(func() {
 				process, err := container.Run(garden.ProcessSpec{
 					User: "root",
-					Path: "/usr/sbin/adduser",
+					Path: "adduser",
 					Args: []string{"-D", "bob"},
 				}, garden.ProcessIO{
 					Stdout: GinkgoWriter,
