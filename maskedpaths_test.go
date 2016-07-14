18edb8c18d157e12414af0f741940c8e178a9ec2
Alberto Leal <aleal@pivotal.io>
2016-07-14 17:54:56 +0100

    Only unprivileged containers apply masked paths
    [#125578951]
    
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

--- maskedpaths_test.go
+++ maskedpaths_test.go
@@ -10,30 +10,37 @@ import (
 
 var _ = Describe("MaskedPaths", func() {
 
-	It("should mask the /proc/* dirs", func() {
-		files := []string{
-			"/proc/timer_stats",
-			"/proc/kcore",
-			"/proc/sched_debug",
-		}
-		for _, file := range files {
-			out := gbytes.NewBuffer()
-			process, err := container.Run(garden.ProcessSpec{
-				Path: "ls",
-				Args: []string{"-la", file},
-			}, garden.ProcessIO{
-				Stdout: out,
-				Stderr: GinkgoWriter,
-			})
-			Expect(err).ToNot(HaveOccurred())
+	Context("when the container is unprivileged", func() {
+		BeforeEach(func() {
+			privilegedContainer = false
+		})
 
-			Expect(process.Wait()).To(Equal(0))
+		It("should mask the /proc/* dirs", func() {
+			files := []string{
+				"/proc/timer_stats",
+				"/proc/kcore",
+				"/proc/sched_debug",
+			}
+			for _, file := range files {
+				out := gbytes.NewBuffer()
+				process, err := container.Run(garden.ProcessSpec{
+					Path: "ls",
+					Args: []string{"-la", file},
+				}, garden.ProcessIO{
+					Stdout: out,
+					Stderr: GinkgoWriter,
+				})
+				Expect(err).ToNot(HaveOccurred())
 
-			expectedFilePermissions := "crw-rw-rw-"
-			expectedMajorVersion := "1,"
+				Expect(process.Wait()).To(Equal(0))
 
-			Expect(out.Contents()).To(ContainSubstring(expectedFilePermissions))
-			Expect(out.Contents()).To(ContainSubstring(expectedMajorVersion))
-		}
+				expectedFilePermissions := "crw-rw-rw-"
+				expectedMajorVersion := "1,"
+
+				Expect(out.Contents()).To(ContainSubstring(expectedFilePermissions))
+				Expect(out.Contents()).To(ContainSubstring(expectedMajorVersion))
+			}
+		})
 	})
+
 })
