d0dad1acbce84b346912b2ebc08320414a4b673b
Petar Petrov <pppepito86@gmail.com>
2017-02-15 17:05:43 +0200

    Move aufs related test to gqt
    [#136611939]

--- rootfs_test.go
+++ rootfs_test.go
@@ -68,23 +68,10 @@ var _ = Describe("Rootfses", func() {
 				})
 
 				Expect(err).ToNot(HaveOccurred())
-
 				process.Wait()
 				Expect(stdout).To(gbytes.Say("foo"))
 			})
 		})
 
-		Context("and the image resides in a private docker registry", func() {
-			It("should return a nice error", func() {
-				_, err := gardenClient.Create(garden.ContainerSpec{
-					Image: garden.ImageRef{
-						URI:      "",
-						Username: "imagepluginuser",
-						Password: "secretpassword",
-					},
-				})
-				Expect(err).To(MatchError(ContainSubstring("private docker registries are not supported")))
-			})
-		})
 	})
 })
