65d1bbb4976400947666ef7389529c16bf60baf0
Will Martin <wmartin@pivotal.io>
2017-04-28 17:14:49 +0100

    Relax private image assertion failure for Groot
    [#144511567]

--- rootfs_test.go
+++ rootfs_test.go
@@ -93,7 +93,7 @@ var _ = Describe("Rootfses", func() {
 				})
 
 				It("fails", func() {
-					Expect(containerCreateErr).To(MatchError(ContainSubstring("resource is not authorized")))
+					Expect(containerCreateErr).NotTo(Succeed())
 				})
 			})
 		})
