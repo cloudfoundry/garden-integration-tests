533e91d9895d4e3dc00df65fbf7de55573223035
Ed King <eking@pivotal.io>
2018-01-19 17:29:53 +0000

    Add improved edge case test
    [#154242239]

--- edgecase/edge_case_test.go
+++ edgecase/edge_case_test.go
@@ -1,7 +1,6 @@
 package edgecase_test
 
 import (
-	"sync"
 	"time"
 
 	"code.cloudfoundry.org/garden"
@@ -9,8 +8,15 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-// These tests are slow, and prone to false positives if we were to regress on
-// the functionality under test.
+// This test require a hacked version of runc to run.
+// The hacked runc is configured to misbehave in certain circumstances,
+// specifically to "hang" (i.e. sleep) before opening the exec fifo.
+// This allows us to test a race condition during pea creation and container
+// destruction.
+// See the "gats-edgecases.yml" CI task for details on how the hacked runc is
+// built.
+// As a result of requiring a hacked runc, the edge case suite cannot easily
+// be run locally without manual intervention.
 var _ = Describe("Edge cases", func() {
 	var (
 		container garden.Container
@@ -23,65 +29,38 @@ var _ = Describe("Edge cases", func() {
 		Expect(err).NotTo(HaveOccurred())
 	})
 
-	AfterEach(func() {
-		Expect(gardenClient.Destroy(container.Handle())).To(Succeed())
-	})
-
-	Context("when creating a pea and destroying the sandbox at the same time", func() {
-		test := func(wg *sync.WaitGroup, peaCreationDuration time.Duration) {
-			defer wg.Done()
-			ctr, err := gardenClient.Create(garden.ContainerSpec{
-				Image: peaImage,
-			})
+	Describe("race condition when creating a pea and destroying the sandbox", func() {
+		BeforeEach(func() {
+			// download the pea image to ensure it's in the cache
+			ctr, err := gardenClient.Create(garden.ContainerSpec{Image: peaImage})
 			Expect(err).NotTo(HaveOccurred())
+			Expect(gardenClient.Destroy(ctr.Handle())).To(Succeed())
+		})
 
-			go ctr.Run(
-				garden.ProcessSpec{
-					Path:  "sleep",
-					Args:  []string{"30"},
-					Image: peaImage,
-				},
-				garden.ProcessIO{
-					Stdout: GinkgoWriter,
-					Stderr: GinkgoWriter,
-				},
-			)
-
-			doneDeleting := make(chan struct{})
+		It("doesn't hang and/or return 'container init still running' error", func() {
+			doneRunningPea := make(chan struct{})
 			go func() {
-				defer GinkgoRecover()
-				defer close(doneDeleting)
-				time.Sleep(peaCreationDuration)
-				Expect(gardenClient.Destroy(ctr.Handle())).To(Succeed())
+				// this will hang due to misbehaving runc
+				container.Run(
+					garden.ProcessSpec{
+						Path:  "sh",
+						Args:  []string{"-c", "echo i-am-pea"},
+						Image: peaImage,
+					},
+					garden.ProcessIO{
+						Stdout: GinkgoWriter,
+						Stderr: GinkgoWriter,
+					})
+				close(doneRunningPea)
 			}()
 
-			<-doneDeleting
-		}
+			time.Sleep(time.Second * 3) // ensure enough time for the runc process to have been started
+			Expect(gardenClient.Destroy(container.Handle())).To(Succeed())
 
-		// https://www.pivotaltracker.com/story/show/154242239
-		It("is able to destroy", func() {
-			start := time.Now()
-			_, err := container.Run(
-				garden.ProcessSpec{
-					Path:  "sleep",
-					Args:  []string{"30"},
-					Image: peaImage,
-				},
-				garden.ProcessIO{
-					Stdout: GinkgoWriter,
-					Stderr: GinkgoWriter,
-				},
-			)
-			peaCreationDuration := time.Since(start)
-			Expect(err).NotTo(HaveOccurred())
-
-			for i := 0; i < 5; i++ {
-				wg := &sync.WaitGroup{}
-				for i := 0; i < 50; i++ {
-					wg.Add(1)
-					go test(wg, peaCreationDuration)
-				}
-				wg.Wait()
+			select {
+			case <-doneRunningPea:
+			case <-time.After(time.Second * 1000):
+				Fail("pea creation hung")
 			}
 		})
 	})
