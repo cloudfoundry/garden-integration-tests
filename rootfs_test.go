2975ca8cd438e392f36f98b39878b1bf0ba701e7
Claudia Beresford <cberesford@pivotal.io>
2018-04-11 10:38:08 +0100

    Use with-process-env image
    Signed-off-by: Georgi Sabev <georgethebeatle@gmail.com>

--- rootfs_test.go
+++ rootfs_test.go
@@ -27,11 +27,9 @@ var _ = Describe("Rootfses", func() {
 	})
 
 	Context("when the rootfs path is a docker image URL", func() {
-		BeforeEach(func() {
-			imageRef.URI = "docker:///cfgarden/with-volume-and-process-env"
-		})
 		Context("and the image specifies $PATH", func() {
 			BeforeEach(func() {
+				imageRef.URI = "docker:///cfgarden/with-process-env"
 				skipIfWoot("Groot does not place environemnt variables in the bundle spec yet")
 			})
 
