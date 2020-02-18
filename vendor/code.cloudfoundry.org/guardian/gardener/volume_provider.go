f06adbe933293b586df42505b9eac8ceaeebec97
Yulia Nedyalkova <julianedialkova@hotmail.com>
2020-02-18 16:14:47 +0000

    vendor guardian
    [#171100116]
    
    Co-authored-by: Kieron Browne <kbrowne@pivotal.io>

--- vendor/code.cloudfoundry.org/guardian/gardener/volume_provider.go
+++ vendor/code.cloudfoundry.org/guardian/gardener/volume_provider.go
@@ -14,22 +14,20 @@ import (
 
 const RawRootFSScheme = "raw"
 
-type CommandFactory func(rootFSPathFile string, uid, gid int, mode os.FileMode, recreate bool, paths ...string) *exec.Cmd
+type CommandFactory func(ctrPID, uid, gid int, mode os.FileMode, recreate bool, paths ...string) *exec.Cmd
 
 type VolumeProvider struct {
 	VolumeCreator VolumeCreator
 	VolumeDestroyMetricsGC
-	prepareRootfsCmd func(rootFSPathFile string, uid, gid int, mode os.FileMode, recreate bool, paths ...string) *exec.Cmd
 	commandRunner    commandrunner.CommandRunner
 	ContainerRootUID int
 	ContainerRootGID int
 }
 
-func NewVolumeProvider(creator VolumeCreator, manager VolumeDestroyMetricsGC, prepareRootfsCmd CommandFactory, commandrunner commandrunner.CommandRunner, rootUID, rootGID int) *VolumeProvider {
+func NewVolumeProvider(creator VolumeCreator, manager VolumeDestroyMetricsGC, commandrunner commandrunner.CommandRunner, rootUID, rootGID int) *VolumeProvider {
 	return &VolumeProvider{
 		VolumeCreator:          creator,
 		VolumeDestroyMetricsGC: manager,
-		prepareRootfsCmd:       prepareRootfsCmd,
 		commandRunner:          commandrunner,
 		ContainerRootUID:       rootUID,
 		ContainerRootGID:       rootGID,
@@ -82,38 +80,5 @@ func (v *VolumeProvider) Create(log lager.Logger, spec garden.ContainerSpec) (sp
 		}
 	}
 
-	v.mkdirAndChown(!spec.Privileged, baseConfig)
-
 	return baseConfig, nil
-}
-
-func (v *VolumeProvider) mkdirAndChown(namespaced bool, spec specs.Spec) error {
-	var uid, gid int
-	if namespaced {
-		uid = v.ContainerRootUID
-		gid = v.ContainerRootGID
-	}
-
-	v.mkdirAs(
-		spec.Root.Path, uid, gid, 0755, true,
-		"dev", "proc", "sys",
-	)
-
-	v.mkdirAs(
-		spec.Root.Path, uid, gid, 0777, false,
-		"tmp",
-	)
-
-	return nil
-}
-
-func (v *VolumeProvider) mkdirAs(rootFSPathFile string, uid, gid int, mode os.FileMode, recreate bool, paths ...string) error {
-	return v.commandRunner.Run(v.prepareRootfsCmd(
-		rootFSPathFile,
-		uid,
-		gid,
-		mode,
-		recreate,
-		paths...,
-	))
 }
