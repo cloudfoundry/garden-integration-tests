c380e8792e4928af2d5c22a1ce562676176de26a
Marc Paquette <mpaquette@vmware.com>
2023-11-22 14:06:00 +0000

    Add bin/test.bash
    for running gats locally as standalone binary
    
    Signed-off-by: Amin Jamali <ajamali@vmware.com>

---
+++ bin/test.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+source "$CI_DIR/shared/helpers/filesystem-helpers.bash"
+filesystem_permit_device_control
+filesystem_create_loop_devices 256
+
+configure_gdn
+
+mkdir -p /var/lib/grootfs
+# shellcheck disable=SC2068
+# Double-quoting array expansion here causes ginkgo to fail
+go run github.com/onsi/ginkgo/v2/ginkgo ${@}
