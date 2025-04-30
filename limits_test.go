2edcff26428c07371504d72dad2a52b907c81c05
Amin Jamali <amin.jamali@broadcom.com>
2025-04-30 17:57:23 +0000

    Update ByteSoft and ByteHard
    After using oci-image conversation. This way our volume size is about 416MB.

--- limits_test.go
+++ limits_test.go
@@ -159,8 +159,8 @@ var _ = Describe("Limits", func() {
 			if runtime.GOOS == "windows" {
 				limits.Disk.ByteHard = 100 * 1024 * 1024
 			} else {
-				limits.Disk.ByteSoft = 100 * 1024 * 1024
-				limits.Disk.ByteHard = 100 * 1024 * 1024
+				limits.Disk.ByteSoft = 500 * 1024 * 1024
+				limits.Disk.ByteHard = 500 * 1024 * 1024
 				limits.Disk.Scope = garden.DiskLimitScopeTotal
 			}
 		})
